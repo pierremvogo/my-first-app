03f33be76c857985d92b35851b4f78cd6dcbb269
mvogopierre <pnkolo@software-phone.com>
2018-11-12 15:09:11 +0100

    main, first commit

---
+++ main/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.username
+    def __str__(self):
+        return self.password
+
+
+class School(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
